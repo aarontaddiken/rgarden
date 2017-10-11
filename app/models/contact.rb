d8d836cc4d59c655b08e0cd01231adc8f6b735ac
Aaron Taddiken <aarontaddiken@gmail.com>
2017-10-10 21:32:43 -0600

    accordion control working in contacts index

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,4 +3,5 @@ self.table_name = 'contact'
   has_many :billtos, class_name: 'Contact', foreign_key: "billto_id"
   belongs_to :billto, class_name: "Contact"
   belongs_to :rep
+  has_many :products, foreign_key: 'company_id'
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ApplicationRecord
 self.table_name = "product"
-  belongs_to :company
+  belongs_to :contact, class_name: "Contact", foreign_key: "company_id"
   belongs_to :product_type
 end
