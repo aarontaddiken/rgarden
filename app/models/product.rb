357b47fc85cffc6c1477abb3b2a804152a5e3cef
Aaron Taddiken <aarontaddiken@gmail.com>
2017-10-16 13:10:22 -0600

    format and style order form

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,5 @@ class Product < ApplicationRecord
 self.table_name = "product"
   belongs_to :contact, class_name: "Contact", foreign_key: "company_id"
   has_many :order_items, foreign_key: "product_id"
+
 end
